996f7316447f7c64cba08b4d86389b8095747194
Kkeviiins <mexicankevin2010@hotmail.com>
2018-05-13 19:21:28 -0500

    Avances y correcciones en el proyecti

--- JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_ReporteDetalleCompra.java
+++ JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_ReporteDetalleCompra.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kevin Fabián Cruz Gómez
+ * Formulario de Reporte de Ventas
  */
 package jake_optimizacion_venta;
 
@@ -31,6 +30,7 @@ public class FRM_ReporteDetalleCompra extends javax.swing.JFrame {
      */
     public FRM_ReporteDetalleCompra() {
         initComponents();
+        this.setLocationRelativeTo(null);
         modeloTabla.addColumn("Id_Compra");
         modeloTabla.addColumn("Fecha");
         modeloTabla.addColumn("Total");
--- JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_ReporteVentas.java
+++ JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_ReporteVentas.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kevin Fabián Cruz Gómez
+ * Formulario de Reporte de Ventas
  */
 package jake_optimizacion_venta;
 
@@ -32,6 +31,7 @@ public class FRM_ReporteVentas extends javax.swing.JFrame {
      */
     public FRM_ReporteVentas() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
