cdbf17204dfdc8850a6b4c6a13054c001536df4b
[Kkeviiins] <KevinCruz@MacBook-Air-de-Kevin.local>
2018-05-31 14:22:52 -0500

    Correción de errores menores

--- JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_Compra.java
+++ JAKE_Optimizacion_Venta/src/jake_optimizacion_venta/FRM_Compra.java
@@ -382,11 +382,15 @@ public class FRM_Compra extends javax.swing.JFrame {
     }//GEN-LAST:event_TXT_CantidadKeyTyped
 
     private void TXT_CostoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_TXT_CostoKeyTyped
-        if (TXT_Costo.getText().contains(".")) {
-            char c = evt.getKeyChar();
-            if (c < '0' || c > '9') {
-                evt.consume();
-            }
+        if(!Character.isDigit(evt.getKeyChar()) && evt.getKeyChar()!= '.')  
+        {
+            evt.consume();
+        }
+        if(evt.getKeyChar() == '.' && TXT_Costo.getText().contains("."))
+        {
+	
+        evt.consume();
+
         }
     }//GEN-LAST:event_TXT_CostoKeyTyped
 
